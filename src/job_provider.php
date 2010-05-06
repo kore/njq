0c3d0c0aa29dee3ecd6a8872417ab2776e1fc323
Kore Nordmann <mail@kore-nordmann.de>
2010-05-06 10:16:23 +0200

    - Implemented: Job provider interface

---
+++ src/job_provider.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Native PHP job queue
+ *
+ * This file is part of njq.
+ *
+ * njq is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; version 3 of the License.
+ *
+ * njq is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with njq; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package VCSWrapper
+ * @subpackage Core
+ * @version $Revision: 954 $
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPLv3
+ */
+
+namespace njq;
+
+/*
+ * VCS wrapper abstracted log entry
+ */
+interface JobProvider
+{
+    /**
+     * Returns if the job provider has more jobs.
+     *
+     * Returns true, if there are more jobs available in the job provider and 
+     * false, if the ally have been executed already.
+     * 
+     * @return bool
+     */
+    public function hasJobs();
+
+    /**
+     * Get next job from job provider
+     *
+     * Get the next job from the job provider. Should return a valid callback, 
+     * which will then be called in the forked child.
+     * 
+     * @return Callback
+     */
+    public function getNextJob();
+}
+
